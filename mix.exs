fa2a028bd5149dc271b6fd66ea372ae49fe1790a
Mayra Cruz <velazquez.leonora@gmail.com>
2021-05-29 01:04:02 -0500

    Agregando librerías para documentar con swagger

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule BeerCatalogApi.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_swagger, "~> 0.8"},
+      {:ex_json_schema, "~> 0.5"} # optional
     ]
   end
 
